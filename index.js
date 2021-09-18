72ffd5a6803745958402501ae760b490cddf35d2
Sid Edwards <sid@semantic.dev>
2021-09-17 22:41:26 -0600

    Update

--- index.js
+++ index.js
@@ -21,14 +21,10 @@ const payload = {
     {
       author: {
         name: core.getInput('message-title') || 'Commits received',
-        icon_url: `${githubPayload.issue.user.avatar_url}`
-      },
-      title: `\[${shortTitle(githubPayload.issue.title)} · Issue #${githubPayload.issue.number}\](${escapeMd(githubPayload.issue.html_url)})`,
-      description: `${escapeMd(shortDes(githubPayload.issue.body))}`,
-      footer: {
-        text: `${escapeMd(githubPayload.organization.description)}`,
         icon_url: `${githubPayload.organization.avatar_url}`
-      }
+      },
+      title: `\`\[${shortTitle(githubPayload.issue.title)} · Issue #${githubPayload.issue.number}\]\`(${escapeMd(githubPayload.issue.html_url)})`,
+      description: `${escapeMd(shortDes(githubPayload.issue.body))}`
     }
   ]
 }
