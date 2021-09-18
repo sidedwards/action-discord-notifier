b374baca2d19ea961e749a2cc10145a7fe55ce47
Sid Edwards <sid@semantic.dev>
2021-09-17 21:57:40 -0600

    Update

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ if (!/https:\/\/discord(app|)\.com\/api\/webhooks\/\d+?\/.+/i.exec(webhook)) {
   core.setFailed('The given discord webhook url is invalid. Please ensure you give a **full** url that start with "https://discordapp.com/api/webhooks"')
 }
 
+const shortTitle = (i) => `${i.substr(0, 55)}...`
+const shortDes = (i) => `${i.substr(0, 250)}...`
 const escapeMd = (str) => str.replace(/([\[\]\\`\(\)])/g, '\\$1')
 
 const { payload: githubPayload } = github.context
@@ -17,8 +19,9 @@ const payload = {
   content: '',
   embeds: [
     {
-      title: core.getInput('message-title') || 'Commits received',
-      description: `[\`\[${escapeMd(githubPayload.issue.title)}\]\`](${githubPayload.issue.url})\n${escapeMd(githubPayload.issue.body)}`
+      author: core.getInput('message-title') || 'Commits received',
+      title: `[${escapeMd(shortTitle(githubPayload.issue.title))} · Issue #${githubPayload.issue.number}](${githubPayload.issue.url})`,
+      description: `${escapeMd(shortDes(githubPayload.issue.body))}`
     }
   ]
 }
